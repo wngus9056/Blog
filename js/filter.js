cb1b8deb0ebb2c78a517d1106aae35f5393d9f88
jhkim <wngus9056@naver.com>
2022-02-22 13:34:38 +0900

    add click filter

--- js/filter.js
+++ js/filter.js
@@ -2,6 +2,7 @@ start();
 
 function start() {
     document.getElementById('searchText').addEventListener('keyup', search);
+    document.querySelector('nav').addEventListener('click', category);
 }
 
 function search() {
@@ -13,8 +14,35 @@ function search() {
         let test_key = row.parentNode.parentNode.parentNode.parentNode
         if (array_key.toUpperCase().indexOf(value) > -1) {
             row.parentNode.parentNode.parentNode.parentNode.style = ''
-        }else{
+        } else {
             row.parentNode.parentNode.parentNode.parentNode.style.display = 'none';
+        }
+    }
+}
+
+function category(e) {
+    const dataValue = 'DATA'
+    const testValue = '비행기'
+    const testValue2 = '하프갤런'
+    const array = document.getElementsByClassName('card__title');
+
+    if (e.target.className == 'fa fa-twitter-square fa-3x') {
+        for (let row of array) {
+            let array_key = row.textContent
+            if (array_key.toUpperCase().indexOf(dataValue) > -1) {
+                row.parentNode.parentNode.parentNode.parentNode.style = ''
+            } else {
+                row.parentNode.parentNode.parentNode.parentNode.style.display = 'none';
+            }
+        }
+    } else if (e.target.className == 'fa fa-instagram fa-3x') {
+        for (let row of array) {
+            let array_key = row.textContent
+            if (array_key.toUpperCase().indexOf(testValue) > -1) {
+                row.parentNode.parentNode.parentNode.parentNode.style = ''
+            } else {
+                row.parentNode.parentNode.parentNode.parentNode.style.display = 'none';
+            }
         }
     }
 }
